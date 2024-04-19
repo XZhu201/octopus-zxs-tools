3dd37b1b58ab64937bd997d6bb5369fc9e0a8b23
XZhu201 <45595240+XZhu201@users.noreply.github.com>
2024-04-19 15:30:45 +0800

    增加读1D动画的程序

---
+++ read_vks_movie_1D/read_vks_movie_1D.m
@@ -0,0 +1,27 @@
+%% input
+index = 0:50:600 ;  %  0:50:77225
+
+%% initialization
+mkdir sv_snapshots_vks;      % figures are saved to this fold for further operation
+Nindex = length(index);
+
+%% run in loop
+for nn = 1:Nindex
+      
+    formatSpec  = 'output_iter/td.%07d/vxc.y=0,z=0' ;
+    str_filename = sprintf(formatSpec, index(nn))
+    
+    DD = importdata(str_filename, ' ', 1);
+    data = DD.data;
+    
+    x = data(:,1);
+    den = data(:,2);
+    
+    figure;
+    plot(x,den); xlabel('x [a.u.]'); ylabel('density');
+    title(str_filename);
+    
+    str_save = sprintf('sv_snapshots_vks/td%07d', index(nn));
+    saveas(gcf,str_save,'png');  
+    
+end % end of nn
